d56e7ac9c3cb6a14aa77ea38d2035113f67ed61f
Faraz Shaikh <farazzshaikh@gmail.com>
2021-06-22 20:41:21 +0530

    Added ability to Read config file

---
+++ functions/src/Config.ts
@@ -0,0 +1,42 @@
+/**
+ * Functions to deal with reading the config file
+ *
+ * Config file path `/gitscraper.json`
+ */
+
+import { promises as fs, existsSync } from "fs";
+import * as path from "path";
+
+type Selector = {
+  [key: string]: string;
+};
+
+type Config = {
+  user: string;
+  selectors?: Selector[];
+};
+
+const defaultSelectors: Selector[] = [];
+
+/**
+ * Reads and parses a config file. The file must be called
+ * `gitscraper.json` and be in the root directory.
+ * @returns {Config} A config object
+ */
+export async function readConfig(): Promise<Config> {
+  // Get the path of the config file
+  const p = path.resolve(__dirname, "../gitscraper.json");
+
+  // if file exists...
+  if (existsSync(p)) {
+    // ...fetch it as JSON
+    const config: Config = JSON.parse(await fs.readFile(p, "utf8"));
+    // ...check if the properties exists
+    if (config.user === undefined) throw new Error(`You must specify a user in the config file.`);
+    if (config.selectors === undefined) config.selectors = defaultSelectors;
+    // ...return the config
+    return config;
+  } else {
+    throw new Error(`Config File does not exist.`);
+  }
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,14 @@
-import * as functions from "firebase-functions";
+// import * as functions from "firebase-functions";
+import { readConfig } from "./Config";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
+
+(async () => {
+  console.log(await readConfig());
+})();
