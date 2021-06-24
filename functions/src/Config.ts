e35e632266f49954f337d818b03564b4990a3e56
Faraz Shaikh <farazzshaikh@gmail.com>
2021-06-24 23:57:37 +0530

    Cleaned up Config.js

--- functions/src/Config.ts
+++ functions/src/Config.ts
@@ -7,48 +7,6 @@
 import { promises as fs, existsSync } from "fs";
 import * as path from "path";
 
-const deepMapKeys = (obj: any, fn: any): any =>
-  Array.isArray(obj)
-    ? obj.map((val) => deepMapKeys(val, fn))
-    : typeof obj === "object"
-    ? Object.keys(obj).reduce((acc, current) => {
-        const key = fn(current);
-        const val = obj[current];
-        // @ts-ignore
-        acc[key] = val !== null && typeof val === "object" ? deepMapKeys(val, fn) : val;
-        return acc;
-      }, {})
-    : obj;
-
-const deepForeach = (obj: any, fn: any) => {
-  Object.keys(obj).forEach((key) => {
-    fn(key);
-
-    if (typeof obj[key] === "object") {
-      deepForeach(obj[key], fn);
-    }
-  });
-};
-
-function deepMap(obj: any, cb: any): any {
-  var out = {};
-
-  Object.keys(obj).forEach(function (k) {
-    var val;
-
-    if (obj[k] !== null && typeof obj[k] === "object") {
-      val = deepMap(obj[k], cb);
-    } else {
-      val = cb(obj[k], k);
-    }
-
-    // @ts-ignore
-    out[k] = val;
-  });
-
-  return out;
-}
-
 /**
  * Reads and parses a config file. The file must be called
  * `gitscraper.json` and be in the root directory.
